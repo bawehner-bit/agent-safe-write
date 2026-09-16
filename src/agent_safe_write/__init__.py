498eba79d0598a2e79b8f1e84302b4d153e56d24
bawehner-bit <ba.wehner@gmail.com>
2026-09-16 11:43:46 +0200

    feat: add public Python API

---
+++ src/agent_safe_write/__init__.py
@@ -0,0 +1,25 @@
+from .core import (
+    DriftDetected,
+    Fingerprint,
+    SafeWriteError,
+    UnsafeTarget,
+    WriteReceipt,
+    WriteStatus,
+    fingerprint,
+    plan_write,
+    safe_write,
+    sha256_bytes,
+)
+
+__all__ = [
+    "DriftDetected",
+    "Fingerprint",
+    "SafeWriteError",
+    "UnsafeTarget",
+    "WriteReceipt",
+    "WriteStatus",
+    "fingerprint",
+    "plan_write",
+    "safe_write",
+    "sha256_bytes",
+]
